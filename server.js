484b94d0acaab8fbd814645585486d3836908db8
Rob <43690425+GeekTekRob@users.noreply.github.com>
2020-09-06 02:14:47 -0400

    Compression
    Trying to compress the build down a bit.

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+const compression = require('compression')
 const express = require('express');
-
 const app = express();
+
+app.use(compression())
 
 app.use(express.static('./dist/gfn-library-catalog'));
 
