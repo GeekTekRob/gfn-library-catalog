0401f6138df8e79acd46bc6da38377f0fc268006
Rob <43690425+GeekTekRob@users.noreply.github.com>
2020-09-06 02:24:28 -0400

    removed compression
    compressions causing issues.

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
-const compression = require('compression')
 const express = require('express');
 const app = express();
-
-app.use(compression())
 
 app.use(express.static('./dist/gfn-library-catalog'));
 
