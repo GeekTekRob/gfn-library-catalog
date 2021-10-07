d89b22c66e06cf6f3fae5a4e11652f7438a3ee83
Rob <43690425+GeekTekRob@users.noreply.github.com>
2021-10-07 16:52:17 -0400

    Fix some image sizing
    Set sizes on icons

--- server.js
+++ server.js
@@ -1,17 +1,12 @@
-const compression = require('compression')
-const express = require('express')
-// const limiter = rateLimit({
-//     windowMs: 60 * 1000, 
-//     max: 5
-//   });
-const app = express()
-app.use(compression())
+const compression = require('compression');
+const express = require('express');
+const app = express();
+app.use(compression());
 
 app.use(express.static('./dist/gfn-library-catalog'));
 
-// app.get('/*', limiter, (req, res) =>
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/gfn-library-catalog/'}),
+app.get('/', (req, res) =>
+  res.sendFile('index.html', {root: 'dist/gfn-library-catalog/'}),
 );
 
 app.listen(process.env.PORT || 8080);
