dd3850231b499e31c2f0905d5db48f6d09100f17
Rob <43690425+GeekTekRob@users.noreply.github.com>
2021-10-07 04:55:26 -0400

    Removing limiter
    issue building app, rolling back change

--- server.js
+++ server.js
@@ -1,19 +1,16 @@
 const compression = require('compression')
 const express = require('express')
-const limiter = rateLimit({
-    windowMs: 60 * 1000, 
-    max: 5
-  });
+// const limiter = rateLimit({
+//     windowMs: 60 * 1000, 
+//     max: 5
+//   });
 const app = express()
 app.use(compression())
-const limiter = rateLimit({
-    windowMs: 60 * 1000, 
-    max: 5
-  });
 
 app.use(express.static('./dist/gfn-library-catalog'));
 
-app.get('/*', limiter, (req, res) =>
+// app.get('/*', limiter, (req, res) =>
+app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/gfn-library-catalog/'}),
 );
 
