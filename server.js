7f633b4268632124b8cbc41199ec35cab258f570
Rob <43690425+GeekTekRob@users.noreply.github.com>
2021-10-07 04:50:11 -0400

    Adding Limiter
    Adding a limiter to server

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const compression = require('compression')
 const express = require('express')
+const limiter = rateLimit({
+    windowMs: 60 * 1000, 
+    max: 5
+  });
 const app = express()
 app.use(compression())
 
 
 app.use(express.static('./dist/gfn-library-catalog'));
 
-app.get('/*', (req, res) =>
+app.get('/*', limiter, (req, res) =>
     res.sendFile('index.html', {root: 'dist/gfn-library-catalog/'}),
 );
 
